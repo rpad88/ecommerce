a480e4453a9fe19eba67f9c4fba64172374754a6
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-08-08 20:39:52 -0400

    iniciando o react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/Home.page"
 
 function App() {
+	fetch("http://localhost:3000/products")
+		.then((res) => {
+			return res.json()
+		})
+		.then(console.log)
 
-  return (
-    <>
-      App is render
-    </>
-  )
+
+    
+
+	return (<>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  </>)
 }
 
 export default App
