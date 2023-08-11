522b449386dab2ecc08a1cdbeb87d0b8829bf745
rpad88 <rodolfo.padilha@gmail.com>
2023-08-10 22:04:09 -0400

    feito a estilização

--- src/components/ProuctsList/ProductList.component.jsx
+++ src/components/ProuctsList/ProductList.component.jsx
@@ -1,11 +1,21 @@
-import ProductCardComponent from "./ProductCard/ProductCard.component";
+import { CardGroup } from 'react-bootstrap'
+import ProductCardComponent from './ProductCard/ProductCard.component'
 import * as Styled from './productList.style'
 
-export default function ProductListComponent({sectionName}) {
-  return (
-    <Styled.ProductListWrapper>
-        <Styled.WrapperTitle>{sectionName}</Styled.WrapperTitle>
-        <ProductCardComponent />
-    </Styled.ProductListWrapper>
-  )
+export default function ProductListComponent({ sectionName }) {
+	return (
+		<Styled.ProductListWrapper>
+			<Styled.WrapperTitle>{sectionName}</Styled.WrapperTitle>
+			<Styled.CardGroup >
+				<ProductCardComponent />
+				<ProductCardComponent />
+				<ProductCardComponent />
+				<ProductCardComponent />
+				<ProductCardComponent />
+				<ProductCardComponent />
+				<ProductCardComponent />
+				
+			</Styled.CardGroup>
+		</Styled.ProductListWrapper>
+	)
 }
--- src/components/ProuctsList/productList.style.jsx
+++ src/components/ProuctsList/productList.style.jsx
@@ -1,15 +1,21 @@
-import { styled } from "styled-components";
-
+import { styled } from 'styled-components'
 
 export const ProductListWrapper = styled.div`
-    margin-top: 1rem;
-    border-top: 1px solid #d3d3d3;
+	margin-top: 1rem;
+	border-top: 1px solid #d3d3d3;
 `
 
 export const WrapperTitle = styled.h5`
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    color: #8d8282;
-    font-size: 1.1rem;
-    margin-bottom: 1rem;
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	color: #8d8282;
+	font-size: 1.1rem;
+	margin-bottom: 1rem;
+`
+
+export const CardGroup = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    justify-content: space-evenly;
+    row-gap: 1rem;
 `
