c2a17eb04654c5a88993c647119686c6bcd2d6c1
rpad88 <rodolfo.padilha@gmail.com>
2023-08-11 14:27:14 -0400

    fixed error on filter bestProducts

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -5,30 +5,43 @@ import { useEffect, useState } from 'react'
 import { ProductsService } from '../service/Products.service'
 
 export default function HomePage() {
+	const [products, setProducts] = useState([])
+	const [bestProducts, setBestProducts] = useState([])
 
-  const [products, setProducts] = useState([])
-  const [bestProducts, setBestProducts] = useState([])
+	const getProducts = async () => {
+		const data = await ProductsService.getAllProducts()
+		if (!data) return alert('something get wrong')
+		setProducts(data)
+	}
+
+	const filterBest = () => {
+		const bestList = products.filter((product) => {
+			return product.average_rating > 4.5
+		})
+		setBestProducts(bestList)
+	}
 
 	useEffect(() => {
-		async function getProducts() {
-			const data = await ProductsService.getAllProducts()
-            if(!data) return alert('something get wrong')
-            setProducts(data)
-		}
 		getProducts()
+	}, [])
 
-    const bestList = products.filter(product => product.average_rating > 4.5)
-    setBestProducts(bestList)
-	},[])
-
-
+	//como não é função assíncrona, podemos chamar de forma reduzida
+	useEffect(filterBest, [products])
 
 	return (
 		<>
 			<HeaderComponent />
 			<Container>
-				<ProductListComponent sectionName="ofertas do dia" products={products} />
-				<ProductListComponent sectionName="melhores produtos" products={bestProducts}/>
+				<ProductListComponent
+					sectionName="ofertas do dia"
+					products={products}
+				/>
+				{bestProducts != 0 && (
+					<ProductListComponent
+						sectionName="melhores produtos"
+						products={bestProducts}
+					/>
+				)}
 			</Container>
 		</>
 	)
