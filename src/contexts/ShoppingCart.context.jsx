abced485217dd2d25c14c99584ea6bd5760be678
rpad88 <rodolfo.padilha@gmail.com>
2023-08-12 10:21:48 -0400

    added removeFromCart()

--- src/contexts/ShoppingCart.context.jsx
+++ src/contexts/ShoppingCart.context.jsx
@@ -1,17 +1,75 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState } from 'react'
 
 export const ShoppingCartContext = createContext({
-    quantity: 0,
-    setQuantity: () => {}
+	cartItems: 0,
+	setCartItems: () => {},
 })
 
-export const ShoppingCartProvider = ({children}) => {
-    const [quantity, setQuantity] = useState(0)
+export const ShoppingCartProvider = ({ children }) => {
+	const [cartItems, setCartItems] = useState([])
 
-    return ( 
-        <ShoppingCartContext.Provider value={{quantity, setQuantity}}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
+	function getItemQuantity(id) {
+		return cartItems.find((item) => item.id === id)?.qtyOnCart || 0
+	}
+
+	function increaseCartQuantity(id) {
+		setCartItems((currentItems) => {
+			// if the product doesn`t exists on shopping cart, add them
+			if (currentItems.find((item) => item.id === id) == null) {
+				return [...currentItems, { id, qtyOnCart: 1 }]
+			} else {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, qtyOnCart: item.qtyOnCart + 1 }
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+	function decreaseCartQuantity(id) {
+		setCartItems((currentItems) => {
+			// if the product doesn`t exists on shopping cart, add them
+			if (currentItems.find((item) => item.id === id).qtyOnCart === 1) {
+				return currentItems.filter((item) => item.id != id)
+			} else {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, qtyOnCart: item.qtyOnCart - 1 }
+					} else {
+						return item
+					}
+				})
+			}
+		})
+	}
+
+    function removeFromCart(id) {
+        setCartItems(currentItems => {
+            return currentItems.filter(item => item.id !== id)
+        })
+    }
+
+	const cartQuantity = cartItems.reduce(
+		(quantity, item) => item.qtyOnCart + quantity,
+		0
+	)
+
+	return (
+		<ShoppingCartContext.Provider
+			value={{
+				cartItems,
+				setCartItems,
+				getItemQuantity,
+				increaseCartQuantity,
+				decreaseCartQuantity,
+                removeFromCart,
+                cartQuantity
+			}}
+		>
+			{children}
+		</ShoppingCartContext.Provider>
+	)
 }
