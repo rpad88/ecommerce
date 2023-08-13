c3bfdf5208c62f269940e50c68048ee1c1b5fda9
rpad88 <rodolfo.padilha@gmail.com>
2023-08-12 21:15:55 -0400

    structure done

--- src/pages/ShoppingCart.page.jsx
+++ src/pages/ShoppingCart.page.jsx
@@ -1,9 +1,24 @@
+import { useContext, useEffect } from 'react'
+import CartListComponent from '../components/Cart/CartList/CartList.component'
+import { ShoppingCartContext } from '../contexts/ShoppingCart.context'
+import { Container } from 'react-bootstrap'
+import CartSummaryComponent from '../components/Cart/CartSummary/CartSummary.component'
 
 export default function ShoppingCartPage() {
-  return (
-    <div>
+	const { cartItems } = useContext(ShoppingCartContext)
 
-      Shopping cart page
-    </div>
-  )
+	useEffect(() => {
+		console.log(cartItems)
+	})
+
+	return (
+		<Container className="d-flex mt-3 flex-wrap" style={{justifyContent: 'space-between'}}>
+			<div className='d-flex flex-column gap-3' style={{flexBasis: '74%'}}>
+				{cartItems.map((product) => (
+					<CartListComponent key={product.id} product={product} />
+				))}
+			</div>
+      <CartSummaryComponent />
+		</Container>
+	)
 }
