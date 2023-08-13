360e73c1a89909d2a4f34d555b8e98d8352f2842
rpad88 <rodolfo.padilha@gmail.com>
2023-08-13 19:41:21 -0400

    include SearchProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { ShoppingCartProvider } from './contexts/ShoppingCart.context.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { SearchProvider } from './contexts/Search.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<ShoppingCartProvider>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<SearchProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</SearchProvider>
 		</ShoppingCartProvider>
 	</React.StrictMode>
 )
