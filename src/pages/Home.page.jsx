10588678820eb03344fafa77f40cfc23362b4f9f
rpad88 <rodolfo.padilha@gmail.com>
2023-08-13 20:54:26 -0400

    add findItem function

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,11 +1,14 @@
 import { Container } from 'react-bootstrap'
 import ProductListComponent from '../components/ProuctsList/ProductList.component'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ProductsService } from '../service/Products.service'
+import { SearchContext } from '../contexts/Search.context'
 
 export default function HomePage() {
 	const [products, setProducts] = useState([])
 	const [bestProducts, setBestProducts] = useState([])
+	const { filter } = useContext(SearchContext)
+	const [filtered, setFiltered] = useState()
 
 	const getProducts = async () => {
 		const data = await ProductsService.getAllProducts()
@@ -27,13 +30,30 @@ export default function HomePage() {
 	//como não é função assíncrona, podemos chamar de forma reduzida
 	useEffect(filterBest, [products])
 
+	function findItem() {
+		const result = products.filter((product) => {
+			return product.name.toLowerCase().includes(filter)
+		})
+		setFiltered(result)
+	}
+
+	useEffect(findItem, [filter])
+
 	return (
 		<>
 			<Container>
-				<ProductListComponent
-					sectionName="ofertas do dia"
-					products={products}
-				/>
+				{filter ? (
+					<ProductListComponent
+						sectionName="encontrados"
+						products={filtered}
+					/>
+				) : (
+					<ProductListComponent
+						sectionName="ofertas do dia"
+						products={products}
+					/>
+				)}
+
 				{bestProducts != 0 && (
 					<ProductListComponent
 						sectionName="melhores produtos"
