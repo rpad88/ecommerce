768c437c36ccfcdd5702e07e07d7948f85c6d426
rpad88 <rodolfo.padilha@gmail.com>
2023-08-14 12:41:50 -0400

    NotFoundPage done

--- src/pages/NotFound.page.jsx
+++ src/pages/NotFound.page.jsx
@@ -1,9 +1,28 @@
+import { Button, Card, Col, Container, Image, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 export default function NotFoundPage() {
-  return (
-    <div>
+	const navigate = useNavigate()
 
-      Error 404
-    </div>
-  )
+	return (
+		<Container className='d-flex align-items-center' style={{maxWidth: '850px'}}>
+			<Col sm={12} lg={12}>
+				<Card className="mt-3 p-3 d-flex align-items-center">
+					<Row>
+						<Card.Img src="https://http.dog/404.jpg"/>
+					</Row>
+					<Row>
+						<Button
+							variant="outline-secondary"
+							size="md"
+							className="mt-5"
+							onClick={() => navigate('/home')}
+						>
+							Ir para a home
+						</Button>
+					</Row>
+				</Card>
+			</Col>
+		</Container>
+	)
 }
